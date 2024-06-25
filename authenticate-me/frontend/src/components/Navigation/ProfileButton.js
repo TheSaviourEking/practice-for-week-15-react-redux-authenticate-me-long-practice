0b515d8d28be03b12d9a1d1343e89335fe94af0b
TheSaviourEking <saviour.eking@outlook.com>
2024-06-25 11:37:44 +0100

    chore: add frontend

---
+++ authenticate-me/frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+function ProfileButton({ user }) {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    return (
+        <>
+            <button onClick={openMenu}>
+                {/* <i className="fas fa-user-circle" /> */}
+                <FontAwesomeIcon icon={faCoffee} />
+            </button>
+            {showMenu && (
+                <ul className="profile-dropdown">
+                    <li>{user.username}</li>
+                    <li>{user.email}</li>
+                    <li>
+                        <button onClick={logout}>Log Out</button>
+                    </li>
+                </ul>
+            )}
+        </>
+    );
+}
+
+export default ProfileButton;
